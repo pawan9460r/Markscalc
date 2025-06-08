88ed5a3993ac68ee1992ea4b4b9a2c5490ad7bf4
pawan kumar <pawan.73719@gmail.com>
2025-06-08 08:57:48 +0530

    Markscalc.py

---
+++ Markscalc.py
@@ -0,0 +1,12 @@
+student = input("Student's Name:")
+subject = input("Subject:")
+t = int(input("Total marks:"))
+m = int(input(f"Gain Marks in {subject}:"))
+
+
+print("----------------------")
+
+
+print("Student's name:", student)
+print("Subject:", subject)
+print("Here is his/Her percentage:", (m/t)*100)
